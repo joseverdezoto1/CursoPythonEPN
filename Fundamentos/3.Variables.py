1f577b5953612a4e23a384f240e6675d1f46c3f9
Jose <jose.verdezoto01@epn.edu.ec>
2022-02-06 15:48:45 -0500

    Fundamentos

--- Fundamentos/3.Variables.py
+++ Fundamentos/3.Variables.py
@@ -1,1 +1,15 @@
 #Variables
+
+from argparse import MetavarTypeHelpFormatter
+
+
+edad = 21
+nombre = 'Juan'
+num = 5.55
+mayorEdad = True
+
+print('Tipo de dato de la edad: ',type(edad))
+print('Tipo de dato del nombre: ',type(nombre))
+print('Tipo de dato del numero: ',type(num))
+print('Tipo de dato de mayor de edad: ',type(mayorEdad))
+
