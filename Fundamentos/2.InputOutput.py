5124bab8a16df583dc8f34c130b4e31d88458bce
Jose <jose.verdezoto01@epn.edu.ec>
2022-02-06 15:38:03 -0500

    Fundamentos

---
+++ Fundamentos/2.InputOutput.py
@@ -0,0 +1,15 @@
+#Funcion de entrada y salida
+
+print('Formulario')
+print('Nombre: ', end='\t')
+nombre = input()
+print('Apellido: ', end='\t')
+apellido = input()
+print('Edad: ', end='\t')
+edad = input()
+ciudad = input('Ciudad: \t')
+
+print('El nombre es: ',nombre)
+print('El apellido es: ',apellido)
+print('La edad es: ',edad)
+print('La ciudad es: ',ciudad)
