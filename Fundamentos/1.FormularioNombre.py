2d53057972e3d7787e31f63d4b667e571130bf4e
Jose <jose.verdezoto01@epn.edu.ec>
2022-02-06 14:40:26 -0500

    Inicio

---
+++ Fundamentos/1.FormularioNombre.py
@@ -0,0 +1,12 @@
+#comentario corto
+
+'''
+Comentario largo
+'''
+
+#ENTRADA Y SALIDA DE DATOS
+#Entrada con input, salida con print
+print("Formulario")
+print('Ingresa tu nombre')
+nombre = input()
+print('El nombre es: ', nombre)
