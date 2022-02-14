7626a169802dd47e2fe88dbe4ca7de115ce3c0b3
Jose <jose.verdezoto01@epn.edu.ec>
2022-02-14 00:10:35 -0500

    actualizacion

---
+++ Ejercicios/2.String1.py
@@ -0,0 +1,3 @@
+# Declarar amor, imprimir ramo
+palabra = 'amor'
+print(palabra[3]+palabra[0]+palabra[1]+palabra[2])
--- Fundamentos/6.Strings.py
+++ Fundamentos/6.Strings.py
@@ -1,6 +1,9 @@
 #String, str
 
 #Asignasiones de unica linea
+from unicodedata import numeric
+
+
 x = 'Hola'
 y = ' con todos'
 
@@ -15,8 +18,39 @@ ahi esta el molino
 
 #Operaciones
 cadena1 = 'Python'
-cadena2 = 'es un lenguaje de programacion'
-
+cadena2 = ' es un lenguaje de programacion'
+numero1 = 5
 #Concatenar, unir 2 strings
 
-print(cadena1 + ' ' + cadena2)
+print(cadena1, '-', cadena2, numero1)
+
+print(cadena1 + cadena2 + str(numero1))
+
+cadena3 = cadena1 + cadena2
+print(cadena3)
+
+#Formated strings
+nombre = 'Jose'
+edad = 21
+#1. = f'{variable}'
+print(f'Hola mi nombre es {nombre} y mi edad es {edad}')
+#2. Llama al metodo clase string .format
+print('Hola mi nombre es {} y mi edad es {}'.format(nombre, edad))
+
+#3. llama al metodor formar pero indicando posicion
+print('Hola mi nombre es {0} y mi edad {1}'.format(nombre, edad))
+#Con repeticion
+print('Me llamo {0}, mi nombre es {0}'.format(nombre))
+#4. con reasignacion de variables
+print('Hola mi nombre es {sinNombre} y mi edad es {sinEdad}'.format(sinNombre = 'Jose', sinEdad = edad))
+
+#Indexacion
+#Jose
+#|0|1|2|3|
+
+cadenatexto = 'Curso de programacion'
+print(cadenatexto[0])
+print(cadenatexto[1])
+print(len(cadenatexto))
+print(cadenatexto[-3])
+print(cadenatexto[3]+cadenatexto[4]+cadenatexto[9]+cadenatexto[14])
